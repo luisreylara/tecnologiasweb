688f09750da20c232f4f813ac4d1e962714b4f4d
Luis Rey Lara <luisreylara@hotmail.com>
2025-05-08 13:37:13 -0600

    Create tuplas.ts

---
+++ ejemplos/tuplas/tuplas.ts
@@ -0,0 +1,16 @@
+//función anónima autoinvocada
+(()=>{
+
+
+const heroe = ['Chapulin Colorado',30];
+const hero: [string,number] = ['Spiderman',25];
+
+
+console.log(hero);
+console.log({hero});
+
+
+const trio:[string,number,boolean]=['Santo',35,true];
+console.log({trio});
+
+})()
