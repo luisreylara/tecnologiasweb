926c228b8b298e5ee487e13e94a93c0353fb85c7
Luis Rey Lara <luisreylara@hotmail.com>
2025-05-12 07:43:55 -0600

    Create flechas2.ts

---
+++ ejemplos/ArrowFunctions/flechas2.ts
@@ -0,0 +1,10 @@
+( ()=>{
+
+    let sum = (x: number, y: number): number => {
+        return x + y;
+    }
+    let sum2 = (x: number, y: number): number =>  x + y;
+    
+   console.log(sum2(20, 20)); 
+
+} )()
