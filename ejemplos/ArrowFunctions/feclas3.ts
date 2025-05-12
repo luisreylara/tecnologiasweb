836f9588f418bcc9769d04e0cc33be5123558aca
Luis Rey Lara <luisreylara@hotmail.com>
2025-05-12 07:49:03 -0600

    Create feclas3.ts

---
+++ ejemplos/ArrowFunctions/feclas3.ts
@@ -0,0 +1,9 @@
+( ()=>{
+
+    let getName = (name: string): string => {
+        return name;
+    };
+    let getName2 = (name: string): string => name;
+
+    console.log(getName2("IRT Rules 6A!!"));
+} )()
