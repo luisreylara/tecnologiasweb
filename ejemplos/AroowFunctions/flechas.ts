d0175fd89756391b4470ff70c53970abdf6840c7
Luis Rey Lara <luisreylara@hotmail.com>
2025-05-12 07:32:01 -0600

    Create flechas.ts

---
+++ ejemplos/AroowFunctions/flechas.ts
@@ -0,0 +1,24 @@
+( ()=>{
+
+const hola = function() {
+        return "Hola UPJR!";
+}
+
+const hola2 = () => {
+    return "Hola UPJR v2";
+}
+
+const hola3 = () =>  "Hola UPJR v2";
+
+const hola4 = (nombre:string) => ` Hola ${nombre} `;
+
+const suma = (num1:number, num2:number) => num1+num2;
+const resta = (num1:number, num2:number) => num1-num2;
+const multi = (num1:number, num2:number) => num1*num2;
+const divi = (num1:number, num2:number) => num1/num2;
+
+const suma2 = function(num1:number, num2:number){
+    return num1+num2;
+}
+
+} )()
