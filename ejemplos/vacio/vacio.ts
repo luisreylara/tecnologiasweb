71f18661f67f806823f80eabf800fc85f282d5cd
Luis Rey Lara <luisreylara@hotmail.com>
2025-05-19 06:19:05 -0600

    Create vacio.ts

---
+++ ejemplos/vacio/vacio.ts
@@ -0,0 +1,20 @@
+(()=>{
+// undefined no es lo mismo que null
+// void le indica a TypeScript que regresa nada, vacio
+    function llamarBatman(){
+
+    }
+    function llamarBatman2():void{
+      //  return 2;
+
+    }
+    const llamarSuperman=()=>{
+
+    }
+    const llamarSuperman2=():void=>{
+
+    }
+    
+    const respuesta = llamarBatman();
+    console.log(respuesta);
+})()
