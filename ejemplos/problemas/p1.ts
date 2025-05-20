ba8820a2b86c795ae8db7448f1a004e0db091763
Luis Rey Lara <luisreylara@hotmail.com>
2025-05-20 07:30:00 -0600

    Create p1.ts

---
+++ ejemplos/problemas/p1.ts
@@ -0,0 +1,14 @@
+(()=>{
+
+
+    
+ const addTwoNumbers = (a, b) => {
+  return a + b;
+};
+
+ () => {
+  console.log(addTwoNumbers(2, 4)).toEqual(6);
+  console.log(addTwoNumbers(10, 10)).toEqual(20);
+};
+
+})()
