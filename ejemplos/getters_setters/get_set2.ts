1154a352905f5052ee42ab9cde9e6432acfcfa37
Luis Rey Lara <luisreylara@hotmail.com>
2025-05-23 06:06:58 -0600

    Create get_set2.ts

---
+++ ejemplos/getters_setters/get_set2.ts
@@ -0,0 +1,26 @@
+class Person {
+  private _age: number;
+  private _firstName: string;
+  private _lastName: string;
+
+  constructor(age: number, firstName: string, lastName: string) {
+    this._age = age;
+    this._firstName = firstName;
+    this._lastName = lastName;
+  }
+
+  public get age() {
+    return this._age;
+  }
+
+  public set age(theAge: number) {
+    if (theAge <= 0 || theAge >= 200) {
+      throw new Error('The age is invalid');
+    }
+    this._age = theAge;
+  }
+
+  public getFullName(): string {
+    return `${this._firstName} ${this._lastName}`;
+  }
+}
