898b70c8b37477ac4d9593f146eaca584d094d1b
Luis Rey Lara <luisreylara@hotmail.com>
2025-05-28 07:54:01 -0600

    Create index.ts

---
+++ ejemplos/genericos/index.ts
@@ -0,0 +1,16 @@
+
+import { createPair, funcionGenerica, funcionGenericaFlecha, funcionGenericaFlecha2, imprimirObjeto } from "./genericos/genericos";
+
+// imprimirObjeto(123);
+// imprimirObjeto("Hola mundo");
+// imprimirObjeto(new Date());
+// imprimirObjeto({a:1,b:2,c:3});
+// imprimirObjeto([1,2,3,4,5]);
+
+
+// console.log(funcionGenerica(3.1416));
+// console.log(funcionGenericaFlecha(3.14).toFixed(2))
+// console.log(funcionGenericaFlecha2("hola mundo").charAt(0));
+// console.log(funcionGenerica({a:1,b:2,c:3}));
+
+console.log(createPair(53,"luis"));
