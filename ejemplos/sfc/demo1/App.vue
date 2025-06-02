1cfb169cd1221b8a24bc175b9ceb7b3e6677f7ee
Luis Rey Lara <luisreylara@hotmail.com>
2025-06-02 10:19:04 -0600

    Create App.vue

---
+++ ejemplos/sfc/demo1/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>{{ saludo }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const saludo = 'Hola UPJR'
+</script>
+
+<style scoped>
+h1 {
+  text-transform: uppercase;
+  color: red;
+}
+</style>
