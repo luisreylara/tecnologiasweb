1772a5af08e3299481d8beea07a641b505a757e3
Luis Rey Lara <luisreylara@hotmail.com>
2025-06-13 05:48:15 -0600

    Create App.vue

---
+++ ejemplos/guitar/guitar1/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <guitarra/>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+  import guitarra from './components/guitar-total.vue';
+
+</script>
+
+<style scoped>
+
+</style>
